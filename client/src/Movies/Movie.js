bad72655b88bdc573ac47c10d6ca7a04e2c09c25
DanielleMathey95 <danielle.mathey@yahoo.com>
2021-02-10 17:25:09 -0800

    Took out unnecessary import of Route

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import {useParams, Route} from 'react-router-dom';
+import {useParams} from 'react-router-dom';
 import axios from 'axios';
 
 export default function Movie(props) {
